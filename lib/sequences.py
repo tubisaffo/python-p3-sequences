0aeecc1b8034137252fa0b229433eb3ef9dca248
tubisaffo <tubisaffo@gmail.com>
2024-06-20 03:01:14 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        print(fib_sequence)
